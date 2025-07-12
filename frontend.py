f97129b62bf5377c56360ba4cb27de3daa9b4964
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-07-12 23:38:32 +0530

    answer and query frontend of chatot setup

--- frontend.py
+++ frontend.py
@@ -9,3 +9,19 @@ uploaded_file=st.file_uploader(
     accept_multiple_files=False
 )
 
+
+user_query=st.text_area('enter your prompt or query u r facing',height=150,placeholder='ask anaything any query u r facing')
+
+ask_question_btn=st.button('ask ai lawyer')
+
+if ask_question_btn:
+
+    if uploaded_file:
+        st.chat_message('user').write(user_query)
+
+    else:
+        st.error('kindly upload a pdf file to ask question from ')
+    
+    #RAG pipeline 
+    fixed_response='hi this is fixed response'
+    st.chat_message('ai lawyer').write(fixed_response)
